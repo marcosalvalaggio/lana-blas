848ffb8308179cd85c733955c761312761fabe5d
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-06-20 13:57:52 +0200

    wip

--- examples/main.py
+++ examples/main.py
@@ -4,14 +4,14 @@ import numpy as np
 
 # lab
 s = time.monotonic()
-result = lb.multiply_matrices(2000)
+result = lb.multiply_matrices(4096)
 e = time.monotonic()
 t = e - s
 print(f"lanablas matmul: {t:.2f} sec.")
 
 # numpy 
-a = np.eye(2000)
-b = np.eye(2000)
+a = np.eye(4096)
+b = np.eye(4096)
 s = time.monotonic()
 result = a @ b
 e = time.monotonic()
