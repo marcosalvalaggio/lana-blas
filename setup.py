94a45496c8fb09ba021d1b8811fe65de32a82494
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-06-20 11:08:23 +0200

    wip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 module_extension = Extension(
     'matrix_extension',
-    sources=['matrix.c'],
+    sources=['src/matrix.c'],
     libraries=['openblas'],  # Link against the BLAS library
     extra_compile_args=['-std=c11'],  # Set the C standard to C11
     include_dirs=['/usr/local/opt/openblas/include'],  # BLAS include directory
@@ -10,8 +10,8 @@ module_extension = Extension(
 )
 
 setup(
-    name='matrix_extension',
-    version='1.0',
+name='lanablas',
+    version='0.0.1',
     description='Extension module for matrix multiplication',
     ext_modules=[module_extension]
 )
